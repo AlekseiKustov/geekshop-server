95958f8bd0ef3b9bee3828690ab5485e23d30024
KustovAleksei <kustovalexej@gmail.com>
2021-04-24 23:22:58 +0300

    test-context added to .gitignore added correct static

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,3 +9,17 @@ def index(request):
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
+
+def test_context(requests):
+    context = {
+        'title': 'test context',
+        'header': 'Добро пожаловать на сайт!',
+        'username': 'Иван Иванов',
+        'products' : [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
+            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
+        ]
+    }
+    return render(requests, 'mainapp/test-context.html', context)
