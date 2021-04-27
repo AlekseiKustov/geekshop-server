16f0c9a8be4881c6671548c3efcd4ee9bb794987
KustovAleksei <kustovalexej@gmail.com>
2021-04-27 16:47:40 +0300

    test-context was deleted from views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -11,17 +11,3 @@ def index(request):
 def products(request):
     context = {'title': 'GeekShop - Каталог'}
     return render(request, 'mainapp/products.html', context)
-
-
-def test_context(requests):
-    context = {
-        'title': 'test context',
-        'header': 'Добро пожаловать на сайт!',
-        'username': 'Иван Иванов',
-        'products' : [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
-            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
-        ]
-    }
-    return render(requests, 'mainapp/test-context.html', context)
