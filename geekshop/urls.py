07cda5f5a12dd8e277854f934423d5d1d9d94897
KustovAleksei <kustovalexej@gmail.com>
2021-05-24 02:01:43 +0300

    something going wrong :(

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-from mainapp.views import index, products
+from mainapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('users/', include('authapp.urls', namespace='users')),
+    path('baskets/', include('basketapp.urls', namespace='baskets')),
+    path('admin-staff/', include('adminapp.urls', namespace='admin_staff')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
